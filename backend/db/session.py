ff42f05aa5cad479300d7cf3801ad22b538c6788
Learn Git <someone@nowhere.com>
2021-09-14 13:32:59 +0300

    section 2 code

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from typing import Generator
+
 from core.config import settings
 
 # for PostGreSQL DataBase Connection
@@ -13,3 +15,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
                        "check_same_thread": False},)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine,)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
